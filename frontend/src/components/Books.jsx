a942e29d66ccdb1570d8abf03c7124c3884f1ed2
Vinsensius Yuda Pratama <pantkrauss@gmail.com>
2024-06-24 19:16:56 +0700

    update

--- frontend/src/components/Books.jsx
+++ frontend/src/components/Books.jsx
@@ -1,23 +1,36 @@
-import { Card, Heading, Image, Text, VStack } from "@chakra-ui/react";
+import { Card, Heading, Image, Text, VStack, Box, HStack } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
-
 
 export default function Books({ id, title, author, image, publisher, year }) {
   return (
     <Link to={`/books/${id}`}>
-    <Card key={id} my={4} p={4} cursor='pointer'>
-      <VStack>
-        <Heading size={"md"}>
-          {title} ({year})
-        </Heading>
-        <Text>{author}</Text>
-        <Image w={24} h={24} src={`http://localhost:8000/${image}`} />
-        <Text>
-          <span>Publisher: </span>
-          {publisher}
-        </Text>
-      </VStack>
-    </Card>
+      <Card
+        key={id}
+        my={4}
+        p={4}
+        cursor="pointer"
+        _hover={{ boxShadow: "lg", transform: "scale(1.02)" }}
+        transition="transform 0.2s ease-in-out"
+      >
+        <HStack align="start">
+          <Image
+            boxSize="100px"
+            objectFit="cover"
+            src={`http://localhost:8000/${image}`}
+            alt={`${title} cover`}
+            mr={4}
+            borderRadius="md"
+          />
+          <VStack align="start" spacing={1}>
+            <Heading size="md">{title} ({year})</Heading>
+            <Text fontWeight="bold">{author}</Text>
+            <Text>
+              <Box as="span" fontWeight="bold">Publisher: </Box>
+              {publisher}
+            </Text>
+          </VStack>
+        </HStack>
+      </Card>
     </Link>
   );
 }
